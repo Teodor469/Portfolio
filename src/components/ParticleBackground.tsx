2ab2f7b991677d2c414377c877a3dcffe2612470
Bebe Kote <lifetroll6@gmail.com>
2025-11-14 19:26:57 +0200

    npm run build

--- src/components/ParticleBackground.tsx
+++ src/components/ParticleBackground.tsx
@@ -14,7 +14,7 @@ interface Particle {
 const ParticleBackground: React.FC = () => {
     const canvasRef = useRef<HTMLCanvasElement>(null);
     const particlesRef = useRef<Particle[]>([]);
-    const animationRef = useRef<number>();
+    const animationRef = useRef<number>(0);
 
     useEffect(() => {
         const canvas = canvasRef.current;
